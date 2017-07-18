563c0965f0c33affebece509f17004277873b888
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-07-18 18:12:11 +0800

    update match system

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -20,38 +20,49 @@ class MatchController extends Controller
 		$data=json_decode($json,TRUE);
 		$u_id=$data['u_id'];
 		//push to match list
-
-		$usermodel=new UserModel();
-		$matchrange=new MatchRangeModel();
-		$characterModel=new CharacterModel();
-		$chardata=$characterModel->where('u_id',$u_id)->first();
-		$maxLv=$matchrange->max('user_lv_to');
-		$maxStar=$matchrange->max('star_from');
-		$match=$matchrange->where('star_from','<=',$chardata['ch_star'])->where('star_to','>=',$chardata['ch_star'])->first();
-		if($chardata['ch_lv']<$maxLv){
+		$now   = new DateTime;;
+		$dmy=$now->format( 'Ymd' );
+		$data=json_decode($json,TRUE);
+		$loginToday=Redis::HGET('login_data',$dmy.$data['u_id']);
+		$loginTodayArr=json_decode($loginToday);
+		$access_token=$loginTodayArr->access_token;
+		if($access_token==$data['access_token'])
+		{
+     		$usermodel=new UserModel();
+     		$matchrange=new MatchRangeModel();
+     		$characterModel=new CharacterModel();
+     		$chardata=$characterModel->where('u_id',$u_id)->first();
+     		$maxLv=$matchrange->max('user_lv_to');
+     		$maxStar=$matchrange->max('star_from');
+		 	$match=$matchrange->where('star_from','<=',$chardata['ch_star'])->where('star_to','>=',$chardata['ch_star'])->first();
+			if($chardata['ch_lv']<$maxLv){
 			$matchKey='match_below_maxlv_star'.$match['star_from'].'to'.$match['star_to'];
-		}
-		else{
-			$matchKey='match_maxlv_star'.$match['star_from'].'star'.$match['star_to'];
-		}
-		$matchList=Redis::LLEN($matchKey);
-
-		if($matchList==0||!$matchList){
-			Redis::LPUSH($matchKey,$u_id);
-			return "wait in list";
-		}
-		else {
-			$match_uid=Redis::LRANGE($matchKey,0,1);
-			if($matchList==1&&$match_uid[0]==$u_id){
-				return "wait in list";
 			}
 			else{
-				$match_uid=Redis::LPOP($matchKey);
-				$result['match_result']=$match_uid;
-				$response=json_encode($result,TRUE);
-				return $response;
+				$matchKey='match_maxlv_star'.$match['star_from'].'star'.$match['star_to'];
 			}
-		}
+			$matchList=Redis::LLEN($matchKey);
+
+			if($matchList==0||!$matchList){
+				Redis::LPUSH($matchKey,$u_id);
+				return "wait in list";
+			}
+			else {
+				$match_uid=Redis::LRANGE($matchKey,0,1);
+				if($matchList==1&&$match_uid[0]==$u_id){
+					return "wait in list";
+				}
+				else{
+					$match_uid=Redis::LPOP($matchKey);
+					$result['match_result']=$match_uid;
+					$response=json_encode($result,TRUE);
+					return base64_encode($response);
+				}
+			}
         
-    }
+    	}
+    	else{
+ 	    		throw new Exception("there have some error of you access_token");
+ 	    }
+ 	    	
  }
