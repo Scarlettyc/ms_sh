e3e3d457314db43442235bd5e87f4a45eb6fe752
Scarlettyc <yuchen_liu_sh@163.com>
2017-07-19 10:44:30 +0800

    baggage test 0719

--- app/getResource.php
+++ app/getResource.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\UserBaggageResModel;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Exception;
+use App\Exceptions\Handler;
+use Illuminate\Http\Response;
+use Carbon\Carbon;
+use DateTime;
+
 function getResource($baggage_u_id)
 {
 	if(isset($baggage_u_id))
