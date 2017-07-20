5ecf2f65668c0c947eb97aef54c2156820f90725
Scarlettyc <yuchen_liu_sh@163.com>
2017-07-20 16:36:15 +0800

    baggage change 0720

--- app/Util/ItemInfoUtil.php
+++ app/Util/ItemInfoUtil.php
@@ -111,7 +111,7 @@ class ItemInfoUtil
 				$NormalRes3Quantity = $UserBaggageResModel->where('u_id','=',$u_id)->where('br_id','=',$normal_res_id_3)->pluck('br_quantity');
 				$result['Normal_Resource3_data']['Normal_Resource3_quantity']=$NormalRes3Quantity;
 			}
-			$response=json_encode($result,TRUE);
+			$response=$result;
 		}else{
 			throw new Exception("No Scroll ID");
 			$response=[
@@ -138,7 +138,7 @@ class ItemInfoUtil
 			$EquipmentEffInfo = $EffectionMstModel->where('eff_id','=',$EquipmentEff_id)->first();
 			$result['Equipment_data']['EquipmentEff_info']=$EquipmentEffInfo;
 
-			$response=json_encode($result,TRUE);
+			$response=$result;
 		}else{
 			throw new Exception("Wrong Equipment ID");
 			$response=[
