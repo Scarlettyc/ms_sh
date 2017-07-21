617d8d5af6a078ea794df91e416526d01a58da9a
Scarlettyc <yuchen_liu_sh@163.com>
2017-07-21 11:48:58 +0800

    baggage sell

--- app/Http/Controllers/BaggageController.php
+++ app/Http/Controllers/BaggageController.php
@@ -155,7 +155,7 @@ class BaggageController extends Controller
 		{
 			//$UserBaggageEqModel->where('u_id',$u_id)->where('status','=',0)->where('b_equ_id',$ItemId)->limit(1)->update(['status'=>1,'updatedate'=>$datetime]);
 			$UserCoin=$UserModel->where('u_id',$u_id)->pluck('u_coin');
-			$updateCoin=$UserCoin+$ItemPrice;
+			$updateCoin=$UserCoin['u_coin']+$ItemPrice;
 			$UserModel->where('u_id',$u_id)->update(['u_coin'=>$updateCoin,'updated_at'=>$datetime]);
 			$response="update Equipment";
 		}else if($ItemType === "itemtype_3")
