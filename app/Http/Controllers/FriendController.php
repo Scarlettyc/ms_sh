31544bfb7ac084a8ac480f993c3b4dabdccb2fda
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-07-24 18:56:09 +0800

    update search friend

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -107,8 +107,9 @@ class FriendController extends Controller
 		$friend_list=$usefriend->where('u_id',$data['u_id'])->where('friend_status',1)->get();
 		$friend_user_ids=[];
 		if($friend_list){
-			$response=json_encode($friend_list,TRUE);
-			return $response;
+			$result['friend_list']=$friend_list
+			$response=json_encode($result,TRUE);
+			return base64_encode($response);
 		}
 		else {
 			return "no friend in list";
