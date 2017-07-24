334e8f3cdf468f67f93ad4dd290badc82add2b86
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-07-24 17:32:07 +0800

    add suggest friend

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -40,7 +40,7 @@ class FriendController extends Controller
 		$data=json_decode($json,TRUE);
 		$usermodel=new UserModel();
 		$lastweek=date('Y-m-d H:i:s', strtotime('last week'));
-		$lastweekUser=$usermodel->where('createdate','>=',$lastweek)->take(10)->get();
+		$lastweekUser=$usermodel->where('pass_tutorial',1)->where('createdate','>=',$lastweek)->take(10)->get();
 		$response=json_encode($lastweekUser,TRUE);
 		return $response;
 	}
