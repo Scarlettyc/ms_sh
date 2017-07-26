7c19295cd88ac6a3ac7bbf4800b55e90d49f9043
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-07-26 16:41:29 +0800

    add swoole commond

--- app/Console/Commands/SwooleCommond.php
+++ app/Console/Commands/SwooleCommond.php
@@ -30,6 +30,49 @@ class SwooleCommond extends Command
         parent::__construct();
     }
 
+    public function fire()
+    {   $arg=$this->argumnt("action");
+        switch ($arg) {
+            case 'start':
+                $this->info("swoole observer started");
+                $this->start();
+                break;
+
+            case 'stop':
+                $this->info("stopped");
+                break;
+            case 'restart':
+                $this->info('restarted');
+                break; 
+        }
+
+    }
+
+    private function start()
+    {   
+        $this->serv=new swoole_server("0.0.0.0",9502);
+        $this->serv->set(array(
+            'worker_num'=>8,
+            'daemonize'=>false,
+            'max_request'=>10000,
+            'dispatch_mode'=>2,
+            'debug_mode'=>1
+            ))
+        $handler=App::make('handlers\SwooleHandler');
+        $this->serv->on('Start',array($handler,'onStart'));
+        $this->serv->on('Connect',array($handler,'onConnect'));
+        $this->serv->on('Receive',array($handler,'onReceive'));
+        $this->serv->on('Close',array($handler,'onClose'));
+
+        $this->serv->start();
+
+    }
+    protected function getArguments(){
+        return array(
+                array('action', InputArgument::REQUIRED, 'start|stop|restart' ),
+                );
+    }
+
     /**
      * Execute the console command.
      *
@@ -39,4 +82,5 @@ class SwooleCommond extends Command
     {
         //
     }
+
 }
