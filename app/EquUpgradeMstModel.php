0f45201c3a1f34f660cb0d6acf984edf22dfa2db
Scarlettyc <yuchen_liu_sh@163.com>
2017-07-28 18:04:44 +0800

    baggage check

--- app/EquUpgradeMstModel.php
+++ app/EquUpgradeMstModel.php
@@ -11,6 +11,6 @@ class EquUpgradeMstModel extends Model
 	protected $fillable = ['upgrade_id','equ_id','equ_upgrade_id','r_id_1','rd1_quantity','r_id_2','rd2_quantity','r_id_3','rd3_quantity','r_id_4','rd4_quantity','equ_coin','updatedate','createdate'];
 
 	protected $connection='mysql';
-	protected $table = "Equ_upgrade_mst";
+	protected $table = "Equ_Upgrade_mst";
 
 }
