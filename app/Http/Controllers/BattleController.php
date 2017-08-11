715e0931d410a5d26e5de510645d2145ead36e61
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-08-11 18:34:16 +0800

    udpate battle calculate

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -73,130 +73,223 @@ class BattleController extends Controller
  	    			}
  				}
  			else{
+ 				$battlekey='battle_data'.$match_id.'_'.$u_id;
  				$userData=$characterModel->where('u_id',$u_id)->first();
- 				$enemyData=$characterModel->where('u_id',$enmey_uid)->first();
- 				$this->calculateEffect($data,$userData,$enemyData,$map_id);
+ 				$final=$this->initialEffect($data,$userData,$map_id);
+ 				$redis_battle->LPUSH($battlekey,$final);
+ 				$response=json_encode($final,TRUE);
+
  			}
  	    	
 		}
 	}
-	private function isHit($data,$enemy,$enemyEffect,$userX,$userY,$enemyX,$enemyY){
+	private function isHit($user,$occurTime,$eff,$enemyX1,$enemyX2,$enemyX3,$enemyY,$map_id){
 		$mapTrap=new MapTrapUtil();
  		$stoneprotect=$mapTrap->nearStone($map_id,$userX,$userY,$enemyX,$enemyY);
  		if($stoneprotect){
  			return false;
  		}
  		else {
-		$effectX=$enemyEffect['eff_ch_x'];
-		$effectY=$enemyEffect['eff_ch_y'];
-		$effectXtill=abs($userX)+abs($effectX);
-		$effectYtill=abs($userY)+abs($effectY);
-		if(abs($enemyX)<$effectXtill&&abs($enemyY)<$effectYtill){
+ 		$time=time()-$occurTime;
+ 		$userX1=$user['x1'];
+ 		$userX2=$user['x2'];
+ 		$userX3=$user['x3'];
+ 		$userY=$data['y'];
+
+		$effectX=abs($user['x1'])+$eff['eff_skill_spd']*$time;
+		$effectY=abs($userY)+1;
+		$bullet=$eff['eff_bullet_width'];
+		$effectXfrom=$effectX-$bulle;
+
+		if(abs($enemyX1+1)>=$effectXfrom&&abs($enemyY+1)==$effectY){
 			return true;
 		}
-		else {
-			return false;
-		}
-	}
+		return false;
+		
 	}
 
- 	private function calculateEffect($data,$user,$enemy,$map_id){
+ 	private function initialEffect($data,$user,$enemy,$map_id){
 
- 			$userX=$data['x'];
+ 			$userX1=$data['x1'];
+ 			$userX2=$data['x2'];
+ 			$userX3=$data['x3'];
 			$userY=$data['y'];
+
  			$skillMstModel=new SkillMstModel();
  			$effectMstModel=new EffectionMstModel();
  			$mapTrap=new MapTrapUtil();
- 			$trap=$mapTrap->getTrapEff($map_id,$userX,$userY);
- 			$enemyX=$enemy['x'];
-			$enemyy=$enemy['y'];
- 			if($trap){
- 				$visible=0;
- 				if($trap['trap_id']==1){
- 					$visible=1;
- 				}
- 				$trapEffHp=$trap['eff_ch_hp'];
- 				$trapEffHp_per=$trap['eff_ch_hp_per'];
- 				$trapEffspd=$trap['eff_ch_spd'];
- 				$trapEffspd_per=$trap['eff_ch_spd_per'];
- 			}
 
- 			$user_skill=$skillMstModel->where('skill_id',$data['skill_id'])->first();
- 			$enemy_skill=$skillMstModel->where('skill_id',$enemy['skill_id'])->first();
- 			if($user_skill['self_eff_id']){
- 			$user_self_effect=$effectMstModel->where('eff_id',$user_skill['self_eff_id'])->first();
- 			$user_effect=$effectMstModel->where('eff_id',$user_skill['enemy_eff_id'])->first();
- 			$enemy_self_effect=$effectMstModel->where('eff_id',$enemy_skill['self_eff_id'])->first();
- 			$enemy_effect=$effectMstModel->where('eff_id',$enemy_skill['enemy_eff_id'])->first();
+ 			$trap=$mapTrap->getTrapEff($map_id,$userX1,$userX2,$userX3,$userY);
+ 			if(isset($trap)){
+ 				if($trap['trap_id']==1){
+ 					$trap['visible']=0;
+ 				}
+ 				$trap['visible']=1;
  			}
+ 			
+ 			if(array_key_exists('self_eff_id',$data)){
+ 				$user_self_buff=$skillMstModel->where('skill_id',$data['self_eff_id'])->first();
+ 				$selfEff=$effectMstModel->Where('eff_id',$user_self_buff['eff_id'])->first();
+ 				$atkEFF['skill_last']=$selfEff['eff_skill_x']/$selfEff['eff_skill_spd'];
+ 				$user['eff'][time()]['self_eff']=$selfEff;
+ 			}
+ 			if(array_key_exists('enemy_eff_id',$data)){
+ 				$user_ef=$skillMstModel->where('skill_id',$data['enemy_eff_id'])->first();
+ 				$atkEff=$effectMstModel->Where('eff_id',$user_ef['eff_id'])->first();
+ 				$atkEFF['skill_last']=$atkEff['eff_skill_x']/$atkEff['eff_skill_spd'];
+ 				$user['eff'][time()]['atk_eff']=$atkEff;
+ 			}				
+      			$user_hp=$user['ch_hp'];
+       			$user_atk=$user['ch_atk'];
+       			$user_def=$user['ch_def'];
+       			// $user_res=$user['res'];
+       			$user_crit=$user['ch_crit'];
+       			$user_cd=$user['ch_cd'];
+       			$user_speed=$user['ch_spd'];      
+
+     			$defindModel=new DefindMstModel();
+     			$defValue=$defindModel->where('defind_id',8);
+     			$Usercritical=$this->getCritical();
+     			$userDMG=$user_atk*$Usercritical*(1-(1-$enemy_def*$defValue['value1'])/(1+$enemy_def*$defValue['value1']));
+     			$userResult['ch_hp']=$user_hp;
+    			$userResult['ch_atk']=$user_atk;
+    			$userResult['ch_def']=$user_def;
+    			$userResult['ch_crit']=$user_crit;
+    			$userResult['ch_cd']=$user_cd;
+    			$userResult['ch_spd']=$user_speed;
+    			$userResult['time']=time();
+    			$userResult['location']=['x1'=>$userX1,'x2'=>$userX2,'x3'=>$userX3,'y'=>$userY];
+    			$userResult['trap']=$trap;
+
+ 				return ['user_result'=>$userResult];
+ 		}
+
+
+ 	private function calculateEffect($data,$user,$enemy,$map_i){
+
+ 			$userX1=$data['x1'];
+ 			$userX2=$data['x2'];
+ 			$userX3=$data['x3'];
+			$userY=$data['y'];
+
+ 			$enemyX1=$enemy['x1'];
+ 			$enemyX2=$enemy['x2'];
+ 			$enemyX3=$enemy['x3'];
+			$enemyY=$enemy['y'];
+
 
 			$user_hp=$user['ch_hp'];
- 			$user_atk=$user['ch_atk'];
- 			$user_def=$user['ch_def'];
- 			// $user_res=$user['res'];
- 			$user_crit=$user['ch_crit'];
- 			$user_cd=$user['ch_cd'];
- 			$user_speed=$user['ch_spd'];
+       		$user_atk=$user['ch_atk'];
+       		$user_def=$user['ch_def'];
+       			// $user_res=$user['res'];
+       		$user_crit=$user['ch_crit'];
+       		$user_cd=$user['ch_cd'];
+       		$user_speed=$user['ch_spd'];   
+			$defindModel=new DefindMstModel();
+			$defValue=$defindModel->where('defind_id',13);
 
- 			$enemy_hp=$enemy['ch_hp'];
- 			$enemy_atk=$enemy['ch_atk'];
- 			$enemy_def=$enemy['ch_def'];
- 			// $enemy_res=$enemy['res'];
- 			$enemy_crit=$enemy['ch_crit'];
- 			$enemy_cd=$enemy['ch_cd'];
- 			$enemy_speed=$enemy['ch_spd'];
- 			
+			$final=[];
 
- 			if($user_self_effect){
- 				$user_hp=($user_hp+$user_self_effect['eff_ch_hp'])*(1+$user_self_effect['eff_ch_hp_per']);
- 				$user_atk=($user_atk+$user_self_effect['eff_ch_atk'])*(1+$user_self_effect['eff_ch_atk_per']);     
- 				$user_def=($user_def+$user_self_effect['eff_ch_def'])*(1+$user_self_effect['eff_ch_def_per']);
-				$user_crit=($user_crit)*(1+$user_self_effect['eff_ch_crit_per']);
- 				$user_cd=($user_cd+$user_self_effect['eff_ch_cd'])*(1+$user_self_effect['eff_ch_cd_per']);
- 				$user_spd=($user_spd)*(1+$user_self_effect['eff_ch_spd_per']);
- 				$user_eff_id=$user_self_effect['eff_id'];
- 				$user_eff_skill_x=$user_self_effect['eff_skill_x'];
- 				$user_eff_skill_y=$user_self_effect['eff_skill_y'];
- 				$user_eff_skill_cd=$user_self_effect['eff_skill_cd'];
- 				$user_eff_skill_spd=$user_self_effect['eff_skill_spd'];
- 				$user_eff_skill_time=time();
- 				$user_x=$data['userx'];
- 			}
- 
- 			if($enemy_effect){
- 				
- 				if($this->isHit($user,$enemy_effect,$user_x,$user_y,$enemy_x,$enemy_y)){
+			if(array_key_exists('eff',$key->$user)){
+				foreach($user['eff'] as $eff){
+				if(array_key_exists('self_eff',$eff)){
+					if($eff['skill_last']<=（$eff['eff_skill_x']/$eff['eff_skill_spd'])){
 
-				$user_hp=($user_hp-$enemy_effect['eff_ch_hp'])*(1-$enemy_effect['eff_ch_hp_per']);
-				$user_atk=($user_atk-$enemy_effect['eff_ch_atk'])*(1-$enemy_effect['eff_ch_atk_per']);
-				$user_def=($user_def-$enemy_effect['eff_ch_def'])*(1-$enemy_effect['eff_ch_def_per']);
-				$user_crit=($user_crit)*(1-$enemy_effect['eff_ch_crit_per']);
-				$user_cd=($user_cd-$enemy_effect['eff_ch_cd'])*(1-$enemy_effect['eff_ch_cd_per']);
-				$user_spd=($user_spd)*(1-$enemy_effect['eff_ch_spd_per']);
+						$user_hp=($user_hp+$eff['eff_ch_hp'])*(1+$eff['eff_ch_hp_per']);
+ 						$user_atk=($user_atk+$eff['eff_ch_atk'])*(1+$eff['eff_ch_atk_per']); 
+ 						$user_def=($user_def+$eff['eff_ch_def'])*(1+$eff['eff_ch_def_per']);
+						$user_crit=($user_crit)*(1+$eff['eff_ch_crit_per']);
+ 						$user_cd=($user_cd+$eff['eff_ch_cd'])*(1+$eff['eff_ch_cd_per']);
+ 						$user_spd=($user_spd)*(1+$eff['eff_ch_spd_per']);
+ 						$user_eff_skill_cd=$eff['eff_skill_cd']-(time()-$key);
+ 						$user_eff_skill_spd=$eff['eff_skill_spd'];
+ 						$skill_last=（$eff['eff_skill_x']/$eff['eff_skill_spd'])-time()-$key;
 
+					if($eff['skill_last']!=（$eff['eff_skill_x']/$eff['eff_skill_spd']){
+						$eff['skill_last']=$eff['eff_skill_x']/$eff['eff_skill_spd']-$eff['skill_last'];
+					 	$final['eff'][$key]['self_eff']=$eff;
+						}
+					}
 				}
 
- 			}
- 			if($enemy_self_effect){
-				$enemy_hp=($enemy_hp+){$enemy_self_effect['eff_ch_hp'])*(1+$enemy_self_effect['eff_ch_hp_per']);
-				$enemy_atk=($enemy_atk+$enemy_self_effect['eff_ch_atk'])*(1+$enemy_self_effect['eff_ch_atk_per']);
-				$enemy_def=($enemy_def+$enemy_self_effect['eff_ch_def'])*(1+$enemy_self_effect['eff_ch_def_per']);
-				$enemy_crit=($enemy_crit)*(1+$enemy_self_effect['eff_ch_crit_per']);
-				$enemy_cd=($enemy_cd+$enemy_self_effect['eff_ch_cd'])*(1+$enemy_self_effect['eff_ch_cd_per']);
-				$enemy_spd=($enemy_spd)*(1+$enemy_self_effect['eff_ch_spd_per']);
- 			}
- 			if($user_effect){
- 				if($this->isHit($enemy,$user_effect,$enemy_x,$enemy_y,$user_x,$user_y)){
- 				$enemy_hp=($enemy_hp-$enemy_self_effect['eff_ch_hp'])*(1-$enemy_self_effect['eff_ch_atk_per']);
- 				$enemy_atk=($enemy_atk-$user_effect['eff_ch_atk'])*(1-$user_effect['eff_ch_atk_per']);
- 				$enemy_def=($enemy_def-$enemy_self_effect['eff_ch_def'])*(1-$enemy_self_effect['eff_ch_def_per']);
- 				$enemy_crit=($enemy_crit)*(1-$enemy_self_effect['eff_ch_crit_per']);
- 				$enemy_cd=($enemy_cd-$enemy_self_effect['eff_ch_cd'])*(1-$enemy_self_effect['eff_ch_cd_per']);
- 				$enemy_spd=($enemy_spd)*(1-$enemy_self_effect['eff_ch_spd_per']);
- 			}
+				if(array_key_exists('atk_eff',$eff)){
+					if($this->isHit($user,$key,$eff,$enemyX1,$enemyX2,$enemyX3,$enemyY,$map_id)){
+						$enemy_hp=($enemy_hp+$eff['eff_ch_hp'])*(1+$eff['eff_ch_hp_per']);
+ 						$enemy_atk=($enemy_atk+$eff['eff_ch_atk'])*(1+$eff['eff_ch_atk_per']);
+ 						$enemy_def=($enemy_def+$eff['eff_ch_def'])*(1+$eff['eff_ch_def_per']);
+						$enemy_crit=($enemy_crit)*(1+$eff['eff_ch_crit_per']);
+ 						$enemy_cd=($enemy_cd+$eff['eff_ch_cd'])*(1+$eff['eff_ch_cd_per']);
+ 						$enemy_spd=($enemy_spd)*(1+$eff['eff_ch_spd_per']);
+ 						$enemy_eff_skill_cd=$eff['eff_skill_cd']-(time()-$key);
+ 						$enemy_eff_skill_spd=$eff['eff_skill_spd'];
+ 						$skill_last=（$eff['eff_skill_x']/$eff['eff_skill_spd'])-time()-$key;
+						}
 
+					}	
+				}
+			}
 
- 			$defindModel=new DefindMstModel();
+			// if(array_key_exists('self_eff_id',$data)){
+ 		// 		$user_self_buff=$skillMstModel->where('skill_id',$data['self_eff_id'])->first();
+ 		// 		$selfEff=$effectMstModel->Where('eff_id',$user_self_buff['eff_id'])->first();
+ 		// 		$atkEFF['skill_last']=$selfEff['eff_skill_x']/$selfEff['eff_skill_spd'];
+ 		// 		$final['eff'][time()]['self_eff']=$selfEff;
+ 		// 	}
+ 		// 	if(array_key_exists('enemy_eff_id',$data)){
+ 		// 		$user_ef=$skillMstModel->where('skill_id',$data['enemy_eff_id'])->first();
+ 		// 		$atkEff=$effectMstModel->Where('eff_id',$user_ef['eff_id'])->first();
+ 		// 		$atkEFF['skill_last']=$atkEff['eff_skill_x']/$atkEff['eff_skill_spd'];
+ 		// 		$final['eff'][time()]['atk_eff']=$atkEff;
+ 		// 	}
+
+ 		// 	if($user_self_effect){
+ 		// 		$user_hp=($user_hp+$user_self_effect['eff_ch_hp'])*(1+$user_self_effect['eff_ch_hp_per']);
+ 		// 		$user_atk=($user_atk+$user_self_effect['eff_ch_atk'])*(1+$user_self_effect['eff_ch_atk_per']);     
+ 		// 		$user_def=($user_def+$user_self_effect['eff_ch_def'])*(1+$user_self_effect['eff_ch_def_per']);
+			// 	$user_crit=($user_crit)*(1+$user_self_effect['eff_ch_crit_per']);
+ 		// 		$user_cd=($user_cd+$user_self_effect['eff_ch_cd'])*(1+$user_self_effect['eff_ch_cd_per']);
+ 		// 		$user_spd=($user_spd)*(1+$user_self_effect['eff_ch_spd_per']);
+ 		// 		$user_eff_id=$user_self_effect['eff_id'];
+ 		// 		$user_eff_skill_cd=$user_self_effect['eff_skill_cd'];
+ 		// 		$user_eff_skill_spd=$user_self_effect['eff_skill_spd'];
+ 		// 		$user_eff_skill_time=time();
+
+ 		// 		$this->isHit($user,$enemy_effect,$userX1,$userX2,$userX3,$user_y,$enemyX1,$enemyX2,$enemyX3,$enemy_y)
+ 		// 	}
+ 
+ 		// 	if($enemy_effect){
+ 				
+ 		// 		if($this->isHit($user,$enemy_effect,$userX1,$userX2,$userX3,$user_y,$enemyX1,$enemyX2,$enemyX3,$enemy_y))
+ 		// 		{
+			// 		$user_hp=($user_hp-$enemy_effect['eff_ch_hp'])*(1-$enemy_effect['eff_ch_hp_per']);
+			// 		$user_atk=($user_atk-$enemy_effect['eff_ch_atk'])*(1-$enemy_effect['eff_ch_atk_per']);
+			// 		$user_def=($user_def-$enemy_effect['eff_ch_def'])*(1-$enemy_effect['eff_ch_def_per']);
+			// 		$user_crit=($user_crit)*(1-$enemy_effect['eff_ch_crit_per']);
+			// 		$user_cd=($user_cd-$enemy_effect['eff_ch_cd'])*(1-$enemy_effect['eff_ch_cd_per']);
+			// 		$user_spd=($user_spd)*(1-$enemy_effect['eff_ch_spd_per']);
+
+			// 	}
+
+ 		// 	}
+ 		// 	if($enemy_self_effect){
+			// 	$enemy_hp=($enemy_hp+){$enemy_self_effect['eff_ch_hp'])*(1+$enemy_self_effect['eff_ch_hp_per']);
+			// 	$enemy_atk=($enemy_atk+$enemy_self_effect['eff_ch_atk'])*(1+$enemy_self_effect['eff_ch_atk_per']);
+			// 	$enemy_def=($enemy_def+$enemy_self_effect['eff_ch_def'])*(1+$enemy_self_effect['eff_ch_def_per']);
+			// 	$enemy_crit=($enemy_crit)*(1+$enemy_self_effect['eff_ch_crit_per']);
+			// 	$enemy_cd=($enemy_cd+$enemy_self_effect['eff_ch_cd'])*(1+$enemy_self_effect['eff_ch_cd_per']);
+			// 	$enemy_spd=($enemy_spd)*(1+$enemy_self_effect['eff_ch_spd_per']);
+ 		// 	}
+ 		// 	if($user_effect){
+ 		// 		if($this->isHit($user,$enemy_effect,$userX1,$userX2,$userX3,$user_y,$enemyX1,$enemyX2,$enemyX3,$enemy_y)){
+ 		// 		$enemy_hp=($enemy_hp-$enemy_self_effect['eff_ch_hp'])*(1-$enemy_self_effect['eff_ch_atk_per']);
+ 		// 		$enemy_atk=($enemy_atk-$user_effect['eff_ch_atk'])*(1-$user_effect['eff_ch_atk_per']);
+ 		// 		$enemy_def=($enemy_def-$enemy_self_effect['eff_ch_def'])*(1-$enemy_self_effect['eff_ch_def_per']);
+ 		// 		$enemy_crit=($enemy_crit)*(1-$enemy_self_effect['eff_ch_crit_per']);
+ 		// 		$enemy_cd=($enemy_cd-$enemy_self_effect['eff_ch_cd'])*(1-$enemy_self_effect['eff_ch_cd_per']);
+ 		// 		$enemy_spd=($enemy_spd)*(1-$enemy_self_effect['eff_ch_spd_per']);
+ 		// 		}
+ 		// 	}
+
  			$defValue=$defindModel->where('defind_id',8);
  			$Usercritical=$this->getCritical();
  			$userDMG=$user_atk*$Usercritical*(1-(1-$enemy_def*$defValue['value1'])/(1+$enemy_def*$defValue['value1']));
@@ -224,7 +317,11 @@ class BattleController extends Controller
 			$enemyResult['ch_stun']=$user_effect['eff_ch_stun'];
 
  		return ['user_result'=>$userResult,'enemy_result'=>$enemyResult];
- 		}
+
+ 	}
+
+ 	private function mapEfft(){
+
  	}
 
 	private function getCritical(){
@@ -240,6 +337,4 @@ class BattleController extends Controller
  			}
  			return $critical;
  		}
-
- 
 }
--- app/Util/MapTrapUtil.php
+++ app/Util/MapTrapUtil.php
@@ -13,33 +13,60 @@ use Illuminate\Support\Facades\Redis;
 class MapTrapUtil
 {
 	//show the quantity and icon for every item in the baggage
-	function getTrapEff($map_id,$x,$y)
+	function getTrapEff($map_id,$x1,$x2,$x3,$y)
 	{
 
 		$mapRelation=new MapTrapRelationMst();
 		$effect=new EffectionMstModel();
 		$trap=new TrapMstModel();
-		$mapData=$mapRelation->Where('map_id',$map_id)->where('trap_x_from','<=',abs($x))->where('trap_x_to','>',abs($x))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))->first();
-		$trapData=$trap->where('trap_id',$mapData['trap_id'])->first();
-		$mapEff=$effect->where('eff_id',$trapData['eff_id'])->first();
-		$mapEff['trap_id']=$mapData['trap_id'];
+		$mapEff=[];
+		$grass=$mapRelation->where(function($query){
+        				$query->Where('map_id',$map_id)->where('trap_id',1)
+        				->where('trap_x_from','>=',abs($x1))
+        				->where('trap_x_to','<=',abs($x2))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
+            				->orWhere(function($query){
+                				->Where('map_id',$map_id)->where('trap_id',1)
+                				->where('trap_x_from','<=',abs($x2))
+                				->where('trap_x_to','>=',abs($x3))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
+           				});
+   					})->first();
+
+         if(!$grass){
+         	$brambles=$mapRelation
+        				->Where('map_id',$map_id)->where('trap_id',1)
+        				->where('trap_x_from','>=',abs($x1))
+        				->where('trap_x_to','<=',abs($x3))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
+   					->first();
+         
+        	if($brambles){
+         		$trapData=$trap->where('trap_id',$brambles['trap_id'])->first();
+        		$mapEff=$effect->where('eff_id',$brambles['eff_id'])->first();
+				$mapEff['trap_id']=$mapData['trap_id'];
+         	}
+     	else{
+				$trapData=$trap->where('trap_id',$grass['trap_id'])->first();
+				$mapEff=$effect->where('eff_id',$grass['eff_id'])->first();
+				$mapEff['trap_id']=$mapData['trap_id'];
+     	}
+     }
+
 		return $mapEff;
 	}
 
 
-	function nearStone($map_id,$x,$y,$enemyX,$enemyY)
+	function nearStone($map_id,$x1,$x2,$x3,$y,$enemyX,$enemyY)
 	{	$mapRelation=new MapTrapRelationMst();
 		$effect=new EffectionMstModel();
 		$trap=new TrapMstModel();
 		$mapData=$mapRelation->where(function($query){
-        				$query->Where('map_id',$map_id)->where('trap_id',3)->where('trap_x_to',abs($x)+1)->where('trap_y_from','<=',abs($y)->where('trap_y_to','>',abs($y)
+        				$query->Where('map_id',$map_id)->where('trap_id',3)->where('trap_x_to',abs($x1)+1)->where('trap_y_from','<=',abs($y)->where('trap_y_to','>',abs($y)-1)
             				->orWhere(function($query){
-                				->Where('map_id',$map_id)->where('trap_id',3)->where('trap_x_to',abs($x)-1)->where('trap_y_from','<=',abs($y)->where('trap_y_to','>',abs($y)
+                				->Where('map_id',$map_id)->where('trap_id',3)->where('trap_x_from',abs($x1)-1)->where('trap_y_from','<=',abs($y)->where('trap_y_to','>',abs($y)-1)
            				});
    					})->first();
 
         if($mapData){
-        	if($mapData['trap_y_from']==abs($enemyY)-1&&$mapData['trap_y_from']==abs($enemyY)+1){
+        	if($mapData['trap_y_from']<=abs($enemyY)&&$mapData['trap_y_from']>=abs($enemy)){
         		return true;
         		}
         	else {
