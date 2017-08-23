e02c047ea2898ba2730f8dd8a1d8088343fdc97f
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-08-23 15:25:04 +0800

    update swoole skill

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -38,35 +38,7 @@ class SwooleCommand extends Command
      */
 
     public function handle()
-    {
-//             $serv = new swoole_server("0.0.0.0/0", 6380, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
-//             $serv->set([
-//                 'worker_num' => 4, # 4个worker
-//                 'task_worker_num' => 4, # 4个task
-//                 'heartbeat_check_interval' => 0.005,
-//                 'heartbeat_idle_time' => 0.1,
-//                 'deamonize' => true,
-//                 ]);
-//             $serv->on('Packet', function ($serv, $data, $clientInfo) {
-
-//             //$serv->task($data);
-//             $battle=new BattleController();
-//             $result=$battle->test($data);
-            
-//             var_dump($clientInfo);
-//               });
-
-
-// //启动服务器
-//             $serv->start();
-//     //     $serv->on('Task', function ($serv, $task_id, $from_id, $data) {
-
-//     //     $battle=new BattleController();
-//     //     $result=$battle->test($data);
-//     //     // $result=$battle->battle($data);
-//     //     for($i = 0 ; $i < 2 ; $i ++ )
-
-       $serv = new swoole_server("0.0.0.0/0", 6380, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
+    {  $serv = new swoole_server("0.0.0.0/0", 6380, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
         $serv->set(array(
             'worker_num'  => 8,
             'daemonize'   => 1, //是否作为守护进程,此配置一般配合log_file使用
@@ -75,18 +47,15 @@ class SwooleCommand extends Command
             'debug_mode' => 1,
             'log_file'    => './storage/logs/swoole.log',
             'heartbeat_check_interval' => 5,
-            'heartbeat_idle_time' => 10, //默认是heartbeat_check_interval的2倍,超过此设置客户端没有回应则强制断开链接
+            'heartbeat_idle_time' => 10, 
         ));
+        $serv->on('Packet', function ($serv, $data, $clientInfo) {
+             $battle=new BattleController();
+             $result=$battle->test($data);
+             $serv->sendto($clientInfo['address'], $clientInfo['port'], "Server ".$result);
+             });
+        $serv->start(); 
 
-//监听数据接收事件
-           $serv->on('Packet', function ($serv, $data, $clientInfo) {
-           $battle=new BattleController();
-           $result=$battle->test($data);
-           $serv->sendto($clientInfo['address'], $clientInfo['port'], "Server ".$result);
-	});
-        //开启
-        $serv->start();
-
-}
+    }
 
 }
