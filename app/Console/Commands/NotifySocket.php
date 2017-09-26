6539cc61dce9d2077c00beace11009975d2f83c6
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-09-26 16:12:35 +0800

    update match system

--- app/Console/Commands/NotifySocket.php
+++ app/Console/Commands/NotifySocket.php
@@ -39,7 +39,38 @@ class NotifySocket extends Command
      */
     public function handle()
     {
-        $serv = new swoole_server("0.0.0.0/0", 6385, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
+        $reqs=array(); //保持客户端的长连接在这个数组里
+        $serv = new swoole_websocket_server("0.0.0.0", 6385,SWOOLE_BASE);
+        $server->set(['worker_num' => 4,'daemonize'   => 0]);
+//如下可以设置多端口监听
+//$server = new swoole_websocket_server("0.0.0.0", 9501, SWOOLE_BASE);
+//$server->addlistener('0.0.0.0', 9502, SWOOLE_SOCK_UDP);
+//$server->set(['worker_num' => 4]);
+
+        $serv->on('Open', function($server, $req) {
+            global $reqs;
+        $reqs[]=$req->fd;
+        $matchController=new MatchController();
+        $matchController->match($req->fd);
+        $serv->sendto($clientInfo['address'], $clientInfo['port'], "Server ".$result);
+
+        //var_dump(count($reqs));//输出长连接数
+    });
+
+        $serv->on('Message', function($server, $frame) {
+        global $reqs;
+        echo "message: ".$frame->data."\n";
+        foreach($reqs as $fd){
+            $server->push($fd, $frame->data);
+            }
+    });
+
+        $serv->on('Close', function($server, $fd) {
+            echo "connection close: ".$fd."\n";
+        });
+
+        $serv->start();
+
         //
     }
 }
