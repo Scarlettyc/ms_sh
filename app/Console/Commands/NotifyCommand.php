379ae53080940a8b15c4f8940946ff41a0d97d2d
Scarlettyc <yuchen_liu_sh@163.com>
2017-09-26 17:25:29 +0800

    update battle

---
+++ app/Console/Commands/NotifyCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use swoole_websocket_server;
+use swoole_server;
+use App\Http\Controllers\MatchController;
+
+class NotifyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+	 $reqs=array(); //保持客户端的长连接在这个数组里
+        $serv = new swoole_websocket_server("0.0.0.0",6385,SWOOLE_BASE);
+        $serv->set(['worker_num' => 4,'daemonize'   => 0]);
+//如下可以设置多端口监听
+//$server = new swoole_websocket_server("0.0.0.0", 9501, SWOOLE_BASE);
+//$server->addlistener('0.0.0.0', 9502, SWOOLE_SOCK_UDP);
+//$server->set(['worker_num' => 4]);
+
+        $serv->on('Open', function($server, $req) {
+            global $reqs;
+        $reqs[]=$req->fd;
+        $matchController=new MatchController();
+        $matchController->testWebsocket($req->fd);
+       // $serv->sendto($clientInfo['address'], $clientInfo['port'], "Server ".$result);
+
+        //var_dump(count($reqs));//输出长连接数
+    });
+
+        $serv->on('Message', function($server, $frame) {
+        global $reqs;
+        echo "message: ".$frame->data."\n";
+        foreach($reqs as $fd){
+            $server->push($fd, $frame->data);
+            }
+    });
+
+        $serv->on('Close', function($server, $fd) {
+            echo "connection close: ".$fd."\n";
+        });
+
+        $serv->start();
+	       
+ 
+    }
+}
