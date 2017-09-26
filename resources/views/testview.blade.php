2c313ee76868d9cee31b8c794d739ed2eab1536f
Scarlettyc <yuchen_liu_sh@163.com>
2017-09-26 16:39:10 +0800

    update match

--- resources/views/testview.blade.php
+++ resources/views/testview.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
 <script type="text/javascript">
-var wsl= 'ws://127.0.0.1:6385'
+var wsl= 'ws://116.62.127.102:6385'
 ws = new WebSocket(wsl);//新建立一个连接
 //如下指定事件处理 
 ws.onopen = function(){ws.send('Test!'); };  
@@ -11,5 +11,8 @@ ws.onclose = function(evt){console.log('WebSocketClosed!');};
 ws.onerror = function(evt){console.log('WebSocketError!');}; 
 
 </script>
+
+
+<p>test test</p>
 </div>
 @endsection
