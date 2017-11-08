69d03a0044f971bd2823bac3422244db48bdcbbf
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-11-08 12:13:56 +0800

    fix error

--- config/cache.php
+++ config/cache.php
@@ -54,7 +54,6 @@ return [
                     'host' => env('MEMCACHED_HOST', '127.0.0.1'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
-                    'persistent' => true,
                 ],
             ],
         ],
