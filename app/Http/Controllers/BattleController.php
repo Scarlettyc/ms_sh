0908cbba7d291f07eed2ec45c3abaab002a28156
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-11-16 13:43:12 +0800

    update character data

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -213,7 +213,7 @@ class BattleController extends Controller
 		}
 			
  			//$user_shield=$userData['ch_shield'];
-			$eqAtt=$eqModel->select('equ_attribute_id')->wherein('equ_id',[$userData['w_id'],$userData['m_id'],$userData['core_id']);
+			$eqAtt=$eqModel->select('equ_attribute_id')->wherein('equ_id',[$userData['w_id'],$userData['m_id'],$userData['core_id']]);
 			$eqAtk= $eqAttr->sum('eff_ch_atk')->wherein('equ_att_id',$eqAtt);
 			$enemyEqAtt=$eqModel->select('equ_attribute_id')->wherein('equ_id',[$userData['w_id'],$enemy['m_id'],$enemy['core_id']);
 			$enemyEqAtk=$eqAttr->sum('eff_ch_atk')->wherein('equ_att_id',$enemyEqAtt);
