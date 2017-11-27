45ed82dccec7ff3a851f723617e182a10eb7346c
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-11-27 12:17:55 +0800

    fix eror

--- app/UserBaggageEqModel.php
+++ app/UserBaggageEqModel.php
@@ -15,7 +15,7 @@ class UserBaggageEqModel extends Model
 	protected $table = "User_Baggage_Eq";
 
 
-	public function equipNewEq($u_id,$b_equ_id,$equ_part,$user_beq_id){
+	public function equipNewEq($u_id,$b_equ_id,$equ_part){
 		$now=new DateTime;
 		$datetime=$now->format( 'Y-m-d h:m:s' );
 
