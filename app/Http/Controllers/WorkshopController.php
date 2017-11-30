7cf7e24c1926822ea5f160a20f9f2b4d68753692
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-11-30 17:08:01 +0800

    update compare equ

--- app/Http/Controllers/WorkshopController.php
+++ app/Http/Controllers/WorkshopController.php
@@ -150,52 +150,52 @@ class WorkshopController extends Controller
 			$result['up_equ']['eff_ch_crit_per']=$comEquAtr['eff_ch_crit_per'];
 			if($eqUpData['rd1_quantity']>0){
 				$r1Qu=$UserBaggageResModel->where('u_id',$u_id)->where('br_id',$eqUpData['r_id_1'])->first();
-				$resource['r_id']=$eqUpData['r_id_1'];
-				$resource['r_qu_need']=$eqUpData['rd1_quantity'];
+				$resource[]['r_id']=$eqUpData['r_id_1'];
+				$resource[]['r_qu_need']=$eqUpData['rd1_quantity'];
 				if($r1Qu['br_quantity']){
-				$resource['r_qu_have']=$r1Qu['br_quantity'];
+				$resource[]['r_qu_have']=$r1Qu['br_quantity'];
 				}
 				else{
-				$resource['r_qu_have']=0;
+				$resource[]['r_qu_have']=0;
 				}
 			}
 			if($eqUpData['rd2_quantity']>0){
 				$r2Qu=$UserBaggageResModel->where('u_id',$u_id)->where('br_id',$eqUpData['r_id_2'])->first();
-				$resource['r_id']=$eqUpData['r_id_2'];
-				$resource['r_qu_need']=$eqUpData['rd2_quantity'];
+				$resource[]['r_id']=$eqUpData['r_id_2'];
+				$resource[]['r_qu_need']=$eqUpData['rd2_quantity'];
 				if($r2Qu['br_quantity']){
-				$resource['r_qu_have']=$r2Qu['br_quantity'];
+				$resource[]['r_qu_have']=$r2Qu['br_quantity'];
 				}else{
-				$resource['r_qu_have']=0;}
+				$resource[]['r_qu_have']=0;}
 			}
 			if($eqUpData['rd3_quantity']>0){
 				$r3Qu=$UserBaggageResModel->where('u_id',$u_id)->where('br_id',$eqUpData['r_id_3'])->first();
-				$resource['r_id']=$eqUpData['r_id_3'];
-				$resource['r_qu_need']=$eqUpData['rd3_quantity'];
+				$resource[]['r_id']=$eqUpData['r_id_3'];
+				$resource[]['r_qu_need']=$eqUpData['rd3_quantity'];
 				if($r3Qu['br_quantity']){
-				$resource['r_qu_have']=$r3Qu['br_quantity'];
+				$resource[]['r_qu_have']=$r3Qu['br_quantity'];
 				}else{
-				$resource['r_qu_have']=0;
+				$resource[]['r_qu_have']=0;
 				}	
 			}
 			if($eqUpData['rd4_quantity']>0){
 				$r4Qu=$UserBaggageResModel->where('u_id',$u_id)->where('br_id',$eqUpData['r_id_4'])->first();
-				$resource['r_id']=$eqUpData['r_id_4'];
-				$resource['r_qu_need']=$eqUpData['rd4_quantity'];
+				$resource[]['r_id']=$eqUpData['r_id_4'];
+				$resource[]['r_qu_need']=$eqUpData['rd4_quantity'];
 				if($r4Qu['br_quantity']){
-				$resource['r_qu_have']=$r4Qu['br_quantity'];
+				$resource[]['r_qu_have']=$r4Qu['br_quantity'];
 				}else{
-				$resource['r_qu_have']=0;
+				$resource[]['r_qu_have']=0;
 				}
 			}
 			if($eqUpData['rd5_quantity']>0){
 				$r5Qu=$UserBaggageResModel->where('u_id',$u_id)->where('br_id',$eqUpData['r_id_5'])->first();
-				$resource['r_id']=$eqUpData['r_id_5'];
-				$resource['r_qu_need']=$eqUpData['rd5_quantity'];
+				$resource[]['r_id']=$eqUpData['r_id_5'];
+				$resource[]['r_qu_need']=$eqUpData['rd5_quantity'];
 				if($r5Qu['br_quantity']){
-				$resource['r_qu_have']=$r5Qu['br_quantity'];
+				$resource[]['r_qu_have']=$r5Qu['br_quantity'];
 				}else{
-				$resource['r_qu_have']=0;
+				$resource[]['r_qu_have']=0;
 				}
 			}
 			$result['resource']=$resource;
