cfdfc0b51603754ebb17c8f32772d66cd3ac1dc3
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-12-01 11:35:10 +0800

    update baggage

--- app/EquipmentMstModel.php
+++ app/EquipmentMstModel.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EquipmentMstModel extends Model
 {
-	protected $fillable = ['equ_id','equ_group','equ_code','equ_name','equ_rarity','equ_lv','equ_attribute_id','special_skill_id','normal_skill_id','equ_part' ,'upgrade_id','equ_price','icon_path','equ_description','createdate','updatedate'];
+	protected $fillable = ['equ_id','equ_group','equ_type','equ_code','equ_name','equ_rarity','equ_lv','equ_attribute_id','special_skill_id','normal_skill_id','equ_part' ,'upgrade_id','equ_price','icon_path','equ_description','createdate','updatedate'];
 
 	protected $connection = 'mysql';
 	protected $table = "Equipment_mst";
