4ac4ce9679a9503ae96404d840a135df15e789ea
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-12-01 15:45:10 +0800

    update baggage

--- app/Http/Controllers/BaggageController.php
+++ app/Http/Controllers/BaggageController.php
@@ -258,11 +258,11 @@ class BaggageController extends Controller
 			
 
 			if($eqDetail['status']==1){
-				if($eqDetail['b_equ_id']==1){
+				if($eqDetail['b_equ_type']==1){
 					$charmodel->update(['w_id'=>$upgradeEquId,'w_bag_id'=>$w_bag_id,'updated_at'=>$datetime]);
 				}
-				else if($eqDetail['b_equ_id']==2){
-					$charmodel->update(['core_id'=>$upgradeEquId,'core_bag_id'=>$w_bag_id,'updated_at'=>$datetime]);
+				else if($eqDetail['b_equ_type']==2){
+					$charmodel->>where('u_id',$u_id)->update(['core_id'=>$upgradeEquId,'core_bag_id'=>$w_bag_id,'updated_at'=>$datetime]);
 				}
 
 			}
