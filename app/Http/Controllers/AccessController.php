1cea564ee687c5569bfa23b5a1631b3caede752d
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-12-05 16:33:39 +0800

    fix update

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -29,8 +29,6 @@ class AccessController extends Controller
 		if($data['uuid'])
 		{  	if(($data['os']='ios'&&strlen($data['uuid'])==40)||($data['os']='android'&&strlen($data['uuid'])==37))
 			{		
-					$data['email']=$data['user_name'];
-					unset($data['user_name']);
 					$userData=$usermodel->createNew($data);	
 			}
 		}
