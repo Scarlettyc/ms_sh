6726dfd214a8eba31c006e2907abaed9b86e1e33
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-12-15 17:24:11 +0800

    update shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -118,6 +118,7 @@ class ShopController extends Controller
 		$item_id=$data['item_id'];
 		$item_type=$data['item_type'];
 		$times=$data['item_times'];
+	
 		$shopData=$inAppModel->select('item_spend','item_min_quantity')->where('item_id',$item_id)->where('item_type',$item_type)->where('start_date','<=',$datetime)->where('end_date','>=',$datetime)->first();
 		$totalSpend=$shopData['item_min_quantity']*$times*$shopData['item_spend'];
 		$userData=$UserModel->where('u_id',$u_id)->first();
@@ -126,7 +127,7 @@ class ShopController extends Controller
 		}
 		else{	$coin=$userData['u_coin']-$totalSpend;
 				$UserModel->where('u_id',$u_id)->update(['u_coin'=>$coin,'updated_at'=>$datetime]);
-				$BaggageUtil->updateBaggageResource($u_id,$item_id,$item_type,$quantity);
+				$BaggageUtil->updateBaggageResource($u_id,$item_id,$item_type,$shopData['item_min_quantity']*$times);
 				return base64_encode("successfully bought resources!");
 		}
 	}
