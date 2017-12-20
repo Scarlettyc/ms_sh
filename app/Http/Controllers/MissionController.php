15e23fa4039336d461125f63c09187c9aa60a0df
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-12-20 17:08:17 +0800

    update mission

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -93,7 +93,7 @@ class MissionController extends Controller
 		$redis_mission=Redis::connection('default');
 		$charModel=new CharacterModel();
 		$chaData=$charModel->where('u_id',$u_id)->first();
-		$missionReward=$missionModel->select('misson_id','item_org_id','item_type','item_quantity',"coin",'exp','times','description')->where('mission_id',$mission_id)->where('user_lv_from','<=',$chaData['ch_lv'])->where('user_lv_to','>',$chaData['ch_lv'])->where('mission_type',1)->where('start_date','<=',$datetime）->where('end_date','>=',$datetime)->first();
+		$missionReward=$missionModel->select('misson_id','item_org_id','item_type','item_quantity',"coin",'exp','times','description')->where('mission_id',$mission_id)->where('user_lv_from','<=',$chaData['ch_lv'])->where('user_lv_to','>',$chaData['ch_lv'])->where('mission_type',1)->where('start_date','<=',$datetime)->where('end_date','>=',$datetime)->first();
 		$key='mission_daily_'.$dmy.'_'.$u_id;
 		if($missionReward['times']==$times){
 			$status=2;
