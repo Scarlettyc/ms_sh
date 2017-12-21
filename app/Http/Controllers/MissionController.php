1747910f3a8c48ee2463b6c157e2900f1d95c35e
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2017-12-21 16:38:11 +0800

    updaet mission

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -144,7 +144,7 @@ class MissionController extends Controller
 			$chaData=$charModel->where('u_id',$u_id)->first();
 			$missionReward=$missionModel->select('mission_id','user_lv_from as lv','item_org_id','item_type','item_quantity','coin','gem','exp','times','description')->where('mission_id',$mission_id)->where('user_lv_from','<=',$chaData['ch_lv'])->where('user_lv_to','>',$chaData['ch_lv'])->where('mission_type',$mission_type)->where('start_date','<=',$datetime)->where('end_date','>=',$datetime)->first();
 			$key='mission_daily_'.$dmy.'_'.$u_id;
-			$missionStatus=$redis_mission->HGET($key,$mission_id);
+			$missionJson=$redis_mission->HGET($key,$mission_id);
 			if($missionJson){
 				$missionStatus=json_decode($missionJson,TRUE);
 				if($missionStatus['times']<$missionReward['times']){
