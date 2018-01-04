77d8bdcf907633233cc4b89402521ebbce28f3fe
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-01-04 14:11:46 +0800

    udpate loadbattle

--- app/Http/Controllers/LoadBattleController.php
+++ app/Http/Controllers/LoadBattleController.php
@@ -118,7 +118,7 @@ class LoadBattleController extends Controller
             $mapId=$matchArr['map_id'];
             $mapData=$mapTrapUtil->getMapData($mapId);
             $result["map_data"]=$mapData;
-            $response=json_encode($mapData,TRUE);
+            $response=json_encode($result,TRUE);
             return  base64_encode($response);
 
         }
