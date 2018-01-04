c9d8cbc7c07096c404d692609925b254625a8104
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-01-04 13:34:16 +0800

    update

--- app/Util/MapTrapUtil.php
+++ app/Util/MapTrapUtil.php
@@ -14,45 +14,45 @@ use Illuminate\Support\Facades\Redis;
 class MapTrapUtil
 {
 	//show the quantity and icon for every item in the baggage
-	function getTrapEff($map_id,$x1,$x2,$x3,$y)
-	{ 
-		$mapRelation=new MapTrapRelationMst();
-		$effect=new EffectionMstModel();
-		$trap=new TrapMstModel();
-		$mapEff=[];
-		$grass=$mapRelation->where(function($query){
-        				$query->Where('map_id',$map_id)->where('trap_id',1)
-        				->where('trap_x_from','>=',abs($x1))
-        				->where('trap_x_to','<=',abs($x2))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
-            				->orWhere(function($query){
-                				Where('map_id',$map_id)
-                                ->where('trap_id',1)
-                				->where('trap_x_from','<=',abs($x2))
-                				->where('trap_x_to','>=',abs($x3))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
-           				})
-   					})->first();
+	// function getTrapEff($map_id,$x1,$x2,$x3,$y)
+	// { 
+	// 	$mapRelation=new MapTrapRelationMst();
+	// 	$effect=new EffectionMstModel();
+	// 	$trap=new TrapMstModel();
+	// 	$mapEff=[];
+	// 	$grass=$mapRelation->where(function($query){
+ //        				$query->Where('map_id',$map_id)->where('trap_id',1)
+ //        				->where('trap_x_from','>=',abs($x1))
+ //        				->where('trap_x_to','<=',abs($x2))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
+ //            				->orWhere(function($query){
+ //                				Where('map_id',$map_id)
+ //                                ->where('trap_id',1)
+ //                				->where('trap_x_from','<=',abs($x2))
+ //                				->where('trap_x_to','>=',abs($x3))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
+ //           				})
+ //   					})->first();
 
-         if(!$grass){
-         	$brambles=$mapRelation
-        				->Where('map_id',$map_id)->where('trap_id',1)
-        				->where('trap_x_from','>=',abs($x1))
-        				->where('trap_x_to','<=',abs($x3))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
-   					->first();
+ //         if(!$grass){
+ //         	$brambles=$mapRelation
+ //        				->Where('map_id',$map_id)->where('trap_id',1)
+ //        				->where('trap_x_from','>=',abs($x1))
+ //        				->where('trap_x_to','<=',abs($x3))->where('trap_y_from','<=',abs($y))->where('trap_y_to','>',abs($y))
+ //   					->first();
          
-        	if($brambles){
-         		$trapData=$trap->where('trap_id',$brambles['trap_id'])->first();
-        		$mapEff=$effect->where('eff_id',$brambles['eff_id'])->first();
-				$mapEff['trap_id']=$mapData['trap_id'];
-         	}
-     	else{
-				$trapData=$trap->where('trap_id',$grass['trap_id'])->first();
-				$mapEff=$effect->where('eff_id',$grass['eff_id'])->first();
-				$mapEff['trap_id']=$mapData['trap_id'];
-     	}
-     }
+ //        	if($brambles){
+ //         		$trapData=$trap->where('trap_id',$brambles['trap_id'])->first();
+ //        		$mapEff=$effect->where('eff_id',$brambles['eff_id'])->first();
+	// 			$mapEff['trap_id']=$mapData['trap_id'];
+ //         	}
+ //     	else{
+	// 			$trapData=$trap->where('trap_id',$grass['trap_id'])->first();
+	// 			$mapEff=$effect->where('eff_id',$grass['eff_id'])->first();
+	// 			$mapEff['trap_id']=$mapData['trap_id'];
+ //     	}
+ //     }
 
-		return $mapEff;
-	}
+	// 	return $mapEff;
+	// }
 
     function isHitStone($map_id,$effX,$effLastX,$effY){
         $mapRelation=new MapTrapRelationMst();
