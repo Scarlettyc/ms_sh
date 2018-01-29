fa36a8572c087d22f91944c8c02388a145b24353
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-01-29 15:50:01 +0800

    update battle

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -94,7 +94,7 @@ class BattleController extends Controller
 		else {
 			foreach ($enemyJson as $key => $each) {
 			   $enmeyData=json_decode($each,TRUE);
-			   $enemy_charData['x']=-($enmeyData['x']);
+			   $enemy_charData['x']=$enmeyData['x'];
 				$enemy_charData['y']=$enmeyData['y'];
 				if(isset($enmeyData['skill_id'])){
 					$enemy_charData['skill_id']=$enmeyData['skill_id'];
