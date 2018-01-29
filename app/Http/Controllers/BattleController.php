dfcf17aa914e74a3816a36f9017dd547aa14fe7e
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-01-29 16:59:35 +0800

    update battle

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -111,7 +111,7 @@ class BattleController extends Controller
  	 					else if($enemy_charData['skill_group']==1){
  	 					$enemy_atk=$enemy_charData['ch_atk']*$atkeff['eff_skill_atk_point']*$atkeff['eff_skill_damage_point']+pow($enemy_charData['ch_lv'],2)*2;
  	 				}
- 	 					$enemyDMG=($atkeff['eff_skill_atk_point']*$enemy_atk+$enemy['eff_skill_base'])*$enemy_charData['ch_crit']*(1-(1-$charData['ch_stam'])/(1+$charData['ch_stam']));
+ 	 					$enemyDMG=($atkeff['eff_skill_atk_point']*$enemy_atk+$enemy_charData['ch_crit']*(1-(1-$charData['ch_stam'])/(1+$charData['ch_stam']));
  	 					$hpMax=$charData['ch_hp_max'];
 						$charData['ch_hp_max']=$hpMax-$enemyDMG;
 				}
