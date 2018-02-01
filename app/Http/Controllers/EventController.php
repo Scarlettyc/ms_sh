0b24dce2ed6b5f9c9b9344c1f0a6502f2246e1a9
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-02-01 12:39:21 +0800

    update event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -22,7 +22,7 @@ class EventController extends Controller
 		$loginTodayArr=json_decode($loginToday);
 		$access_token=$loginTodayArr->access_token;
     	if($access_token==$data['access_token']){
-    		$eventList=$event->select('banner_path','web_path')->where('start_date',<=$datetime)->where('end_date',<=$datetime)->get();
+    		$eventList=$event->select('banner_path','web_path')->where('start_date','<=',$datetime)->where('end_date','>=',$datetime)->get();
     		$reslut['event_list']=$eventList;
     		$response=json_encode($reslut,TRUE);
     		return $response;
