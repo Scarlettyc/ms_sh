68ced146d4d849508105c00096a653a2cce35c7d
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-02-12 12:11:56 +0800

    test battle reward

--- app/BattleSpecialRewardsMst.php
+++ app/BattleSpecialRewardsMst.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BattleSpecialRewardsMst extends Model
 {
-	protected $fillable = ['bs_id','map_id','ranking''item_org_id','item_quantity','item_type','rate_from','rate_to','start_date','end_date','createdate'];
+	protected $fillable = ['bs_id','map_id','ranking','item_org_id','item_quantity','item_type','rate_from','rate_to','start_date','end_date','createdate'];
 
 	protected $connection = 'mysql';
 	protected $table = "Battle_special_rewards_mst";
