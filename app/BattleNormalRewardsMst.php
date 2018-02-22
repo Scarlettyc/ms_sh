622713ed89b685406e04c5b077f03f3eaa67c1ec
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-02-22 16:51:31 +0800

    fix battles

--- app/BattleNormalRewardsMst.php
+++ app/BattleNormalRewardsMst.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BattleNormalRewardsMst extends Model
 {
-	protected $fillable = ['bn_id','map_id','ranking','item_org_id','item_quantity','item_type','start_date','end_date','createdate'];
+	protected $fillable = ['bn_id','map_id','ranking','lv','item_org_id','item_type','item_quantity','item_rate_from','item_rate_to','start_date','end_date','createdate','updated_at'];
 
 	protected $connection = 'mysql';
 	protected $table = "Battle_normal_rewards_mst";
