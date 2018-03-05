a496afb1b1590b7b29d7ca0994542914ecbc9518
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-03-05 17:09:17 +0800

    update equ upgrade

--- app/Http/Controllers/BaggageController.php
+++ app/Http/Controllers/BaggageController.php
@@ -267,6 +267,7 @@ class BaggageController extends Controller
 
 			// $charmodel->where('u_id',$u_id)->update(['w_id'=>$w_bag_id])
 			$w_bag_id=$UserBaggageEqModel->insertGetId(['u_id'=>$u_id,'b_equ_id'=>$upgradeEquId,'b_equ_rarity'=>$upgradeEquInfo['equ_rarity'],'b_equ_type'=>$upgradeEquInfo['equ_type'],'b_icon_path'=>$upgradeEquInfo['icon_path'],'status'=>$eqDetail['status'],'updated_at'=>$datetime,'created_at'=>$datetime]);
+			var_dump($w_bag_id);
 
 			$UserBaggageEqModel->where('user_beq_id',$data['user_beq_id'])->where('u_id',$u_id)->update(['status'=>2,'updated_at'=>$datetime]);	
 				
@@ -274,7 +275,6 @@ class BaggageController extends Controller
 
 
 			if($eqDetail['status']==1){
-
 			
 				if($eqDetail['b_equ_type']==1){
 					$charmodel->where('u_id',$u_id)->update(['w_id'=>$upgradeEquId,'w_bag_id'=>$w_bag_id,'updated_at'=>$datetime]);
