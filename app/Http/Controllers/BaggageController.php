43794622432463113be9e06d21d490c708ff6355
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-03-06 18:50:53 +0800

    add upgrade new logic

--- app/Http/Controllers/BaggageController.php
+++ app/Http/Controllers/BaggageController.php
@@ -235,7 +235,7 @@ class BaggageController extends Controller
 		$user_beq_id=$data['user_beq_id'];
 		$equData=$EquipmentMstModel->select('equ_id','equ_type','equ_code','equ_rarity','equ_lv')->where('equ_id',$equipmentId)->first();
 		$eqDetail=$UserBaggageEqModel->where('u_id',$u_id)->where('user_beq_id',$user_beq_id)->where('b_equ_id',$equipmentId)->first();
-		$upgradeInfo=$EquUpgradeReMstModel->where('equ_id',$equipmentId)->get();
+		$upgradeInfo=$EquUpgradeReMstModel->where('upgrade_id',$equData['upgrade_id'])->get();
 		$ItemInfoUtil->validateResource($u_id,$upgradeInfo,$equData['upgrade_coin']);
 		$upgradeNext=$EquUpgradeReMstModel->where('equ_code','like',$equData['equ_code'].'%')->where('lv',$equData['equ_lv']+1)->first();
 
