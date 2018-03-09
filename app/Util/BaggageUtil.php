381577b4aff49b1db5bbf5d8bbb8e744013ae983
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-03-09 19:57:06 +0800

    fix baggage

--- app/Util/BaggageUtil.php
+++ app/Util/BaggageUtil.php
@@ -379,7 +379,7 @@ class BaggageUtil
 				 $UserModel->where('u_id',$u_id)->update(['u_coin'=>$userData['u_coin']+$reward['item_quantity'],'updated_at'=>$datetime]);
 			}
 			else if($reward['item_type']==7){
-				$UserModel->where('u_idinsertToBaggageu_id)->update(['u_gem'=>$userData['u_gem']+$reward['item_quantity'],'updated_at'=>$datetime]);
+				$UserModel->where('u_id',$u_id)->update(['u_gem'=>$userData['u_gem']+$reward['item_quantity'],'updated_at'=>$datetime]);
 			}
 			// $missionlist[]=$reward['misson_id'];
 		 }
