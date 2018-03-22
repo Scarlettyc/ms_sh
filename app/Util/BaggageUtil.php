29b6ca4ea7326a89c44a8204f80d63f1df439f2e
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-03-22 18:58:02 +0800

    fix error

--- app/Util/BaggageUtil.php
+++ app/Util/BaggageUtil.php
@@ -71,7 +71,7 @@ class BaggageUtil
 			$UserBaggageEqModel=new UserBaggageEqModel();
 				$result=[];
 			$defindMstModel=new DefindMstModel();
-			$baggageWeapon=$UserBaggageEqModel->select('user_beq_id','b_equ_id','b_icon_path')->where('u_id','=',$u_id)->where('status','=',$status)->where('b_equ_type','=',$equ_type)->orderBy('b_equ_rarity','DESC')->orderBy('b_equ_id','DESC')->get();
+			$baggageWeapon=$UserBaggageEqModel->select('user_beq_id','b_equ_id','b_icon_path','b_equ_rarity')->where('u_id','=',$u_id)->where('status','=',$status)->where('b_equ_type','=',$equ_type)->orderBy('b_equ_rarity','DESC')->orderBy('b_equ_id','DESC')->get();
 			foreach ($baggageWeapon as $obj) 
 			{	$arry['baggage_id']=$obj['user_beq_id'];
 				$arry['item_id']=$obj['b_equ_id'];
