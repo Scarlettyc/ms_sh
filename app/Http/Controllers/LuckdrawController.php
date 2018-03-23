daa8210682547f9e4cde818945940c009e9f5c19
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-03-23 17:03:07 +0800

    update rotes

--- app/Http/Controllers/LuckdrawController.php
+++ app/Http/Controllers/LuckdrawController.php
@@ -177,16 +177,20 @@ public function createLuckDraw(Request $request){
 			}else if(!$luck['lk_id']==14||!$luck['lk_id']==28){
 				$redis_key_before=$luck['rate_key'].$i-1;
 				$redis_data=$redisTables->HGET('lucky_draw_rate_table',$redis_key_before);
-				$arrb4=json_decode($redis_data,TRUE);
+				if(!is_null($redis_data))
+				{ $arrb4=json_decode($redis_data,TRUE);
 				$rate_from=$arrb4['rate_to'];
 				$rate_to=round(($luck['weight']/(4049+pow(1.0124,($i-1))))*10000);
+				}
 			}
 			else if($luck['lk_id']==14||$luck['lk_id']==28){
 				$redis_key_before=$luck['rate_key'].($i-1);
 				$redis_data=$redisTables->HGET('lucky_draw_rate_table',$redis_key_before);
+				if(!is_null($redis_data)){
 				$arrb4=json_decode($redis_data,TRUE);
 				$rate_from=$arrb4['rate_to'];
 				$rate_to=10000-$rate_from;
+				}
 			}
 			$arr['rate_from']=$rate_from;
 			$arr['rate_to']=$rate_to;
