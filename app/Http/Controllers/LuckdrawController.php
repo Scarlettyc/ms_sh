d98ad9f88e0ef45fda95c33e3b65344e1aec2999
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-03-28 17:37:38 +0800

    udpate luck draw

--- app/Http/Controllers/LuckdrawController.php
+++ app/Http/Controllers/LuckdrawController.php
@@ -42,7 +42,7 @@ class LuckdrawController extends Controller
 		$defindDiscount=$defindMst->where('defind_id',22)->first();
 		$defindFree=$defindMst->where('defind_id',33)->first();
 
-		$luckData=$luckdraw->select(DB::raw('lk_id,item_id,draw_type ,item_quantity, item_type,if(item_type=3, CONCAT("Scroll_Random_",item_rarilty),"") as sc_img_path'))->where('draw_type',$draw_type)->where('start_date','<=',$date)->where('end_date','>=',$date)->get();
+		$luckData=$luckdraw->select(DB::raw('lk_id,item_id,draw_type ,item_quantity, item_rarilty,item_type,if(item_type=3, CONCAT("Scroll_Random_",item_rarilty),"") as sc_img_path'))->where('draw_type',$draw_type)->where('start_date','<=',$date)->where('end_date','>=',$date)->get();
 		if($draw_type==$defindFree['value1']){
 			$freeDraw=$redisLuck->HGET('luckdrawfree',$dmy.$data['u_id']);
 			if($freeDraw){
