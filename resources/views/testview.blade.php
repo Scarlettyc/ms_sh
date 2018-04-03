bc241c4d6576b00797d68eaf46e57d35ac8f0ad5
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-04-03 16:04:53 +0800

    udpate test view

--- resources/views/testview.blade.php
+++ resources/views/testview.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.app')
-@section('content')
+
 <div class="container">
 <script type="text/javascript">
 var wsl= 'ws://116.62.127.102:6385'
@@ -12,9 +11,5 @@ ws.onerror = function(evt){console.log('WebSocketError!');};
 </script>
 <div class="form-group">  
            <label class="col-lg-4 control-label">{{trans('message.welcome')}}</label>  
-            <div class="col-lg-6">  
-                <input type="text" class="form-control" name="name" value="{{old('name')}}" autofocus/>  
-            </div>  
        </div> 
 </div>
-@endsection
