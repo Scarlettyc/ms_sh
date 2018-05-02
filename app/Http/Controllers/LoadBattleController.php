9571aaae2c06a0c15889b58a163e4528dba396ef
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-02 17:25:47 +0800

    update battle

--- app/Http/Controllers/LoadBattleController.php
+++ app/Http/Controllers/LoadBattleController.php
@@ -86,8 +86,10 @@ class LoadBattleController extends Controller
         $core_skill=$skillModel->select('skill_id','skill_group','skill_damage', 'skill_name','skill_icon','skill_cd','skill_info')->where('equ_id',$core_id)->first();
         $movement_skill=$skillModel->select('skill_id','skill_group','skill_damage', 'skill_name','skill_icon','skill_cd','skill_info')->where('equ_id',$movement_id)->first();
          foreach ($normal_skills as $key =>$eachSkill){
-            $eachSkill['skill_effs']=$this->getEffs($eachSkill);
-            $result['normal_skills'][]=$eachSkill;
+            $eff=$this->getEffs($eachSkill);
+            $tmp=$eachSkill;
+            $tmp['skill_effs']=$eff;
+            $result['normal_skills']=$tmp;
          }
         $special_effs=$this->getEffs($special_skill);
         $core_effs=$this->getEffs($core_skill);
