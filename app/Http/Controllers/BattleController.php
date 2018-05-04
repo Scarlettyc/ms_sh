5cc5552815a76a100bf5f785d5a44979f1e44637
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-04 18:36:12 +0800

    update battle

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -315,6 +315,7 @@ public function battleNew($data,$clientInfo){
 					$charData['ch_crit']=$userData['ch_crit'];
 					$charData['ch_armor']=$userData['ch_armor'];
 					$charData['ch_lv']=$userData['ch_lv'];
+					$charData['skill']=$userData['skill'];
 					if($identity==1){
 					$charData['x']=$x;
 					$charData['y']=$y;
