b520b8f2a0105a59a11e6a312657dcdc146e1b34
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-04 15:39:29 +0800

    update battle

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -385,7 +385,7 @@ public function battleNew($data,$clientInfo){
 			$enemyData['direction']=-($enemyData['direction']);
 		}else{
 			$enemyData['x']=-($enemyData['x']);
-			$enemyData['x2']=-($enemyData['x2']);
+			//$enemyData['x2']=-($enemyData['x2']);
 			$enemyData['direction']=-($enemyData['direction']);
 			$charData['x']=-($charData['x']);
 			$charData['x2']=-($charData['x2']);
