0e90380185641585e2d2f6daa16f82ee1974d6c2
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-07 16:48:08 +0800

    battle update

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -389,7 +389,10 @@ class AttackHitUtil
 			$enemyDMG=$enemy_atk*$critBool;
 			$hpMax=$chardata['ch_hp_max']/(1-$user_def);
 			$chardata['ch_hp_max']=round($hpMax-$enemyDMG);
-			Log::info('myhp: '.$hpMax.'my def '.$user_def.' enemyDMG'.$enemyDMG. 'after_hp'.$chardata['ch_hp_max']);
+			if($chardata['ch_hp_max']<0){
+				$chardata['ch_hp_max']=0;
+			}
+			Log::info('myhp: '.$hpMax.'my def '.$user_def.'enemy_atk'.$enemyData['ch_atk'].'eff_skill_atk_point '.$skillatkEff['eff_skill_atk_point'].' enemyDMG'.$enemyDMG. 'after_hp'.$chardata['ch_hp_max']);
   		}
   		else if ($enemyData['skill']['skill_group']==2){
   			$enemy_atk=$enemyData['ch_atk']*$atkeff['eff_skill_atk_point']+pow($enemy_charData['ch_lv'],2)*2;
