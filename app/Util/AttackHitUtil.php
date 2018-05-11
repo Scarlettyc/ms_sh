ad480b1fc27538605e920a4d3d02f912b7519ddb
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-11 19:54:44 +0800

    updaet attack

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -469,11 +469,4 @@ class AttackHitUtil
 
   }
 
-
-  	private function getMillisecond() {
-		list($t1, $t2) = explode(' ', microtime());     
-		return (float)sprintf('%.0f', (floatval($t1) + floatval($t2)) * 1000);  
-	}
-
-
 }
