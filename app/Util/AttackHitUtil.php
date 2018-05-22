344399b1d21a148d37f3262a13f8b6269415f00d
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-22 20:49:35 +0800

    update battle

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -255,7 +255,7 @@ class AttackHitUtil
             $enemyY_to=$start_y+$effs['TL_y_a'];
       }
         if($y_font<$y_back){
-          if($enemyX_from<=$x_back&&$enemyX_from>=$x_front&&$y_back>=$enemyY_to&&||$enemyX_from>=$x_back&&$enemyX_from<=$x_front&&$y_back>=$enemyY_to){
+          if($enemyX_from<=$x_back&&$enemyX_from>=$x_front&&$y_back>=$enemyY_to||$enemyX_from>=$x_back&&$enemyX_from<=$x_front&&$y_back>=$enemyY_to){
            $hit=true;
         }
         else if($y_font>$y_back){
