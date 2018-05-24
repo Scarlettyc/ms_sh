e97855c0f98313fd28177199488c3ab08dcd0a32
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-24 18:16:25 +0800

    update battle

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -301,7 +301,7 @@ class AttackHitUtil
             // Log::info('damage 2 skill_id'.$skill_id.' enemyX'.$enemyX.' enemyY'.$enemyY.' enemyskillXfrom'.$enemyX_from.' enemyskillXto'.$enemyX_to.' enemyskillYfrom'.$enemyY_from.' enemyskillYto'.$enemyY_to.' enemy_direction'.$enemy_direction.' userxfront'.$x_front.' useryfront'.$y_font.' user_xBack'.$x_back.' user_yBack'.$y_back.' userDirection'.$direction);	
 			   }
          else if(!$hit&&$skill_damage==2&&$current-$occur_time>$effs['eff_duration']){
-           //$redis_battle->HDEL($fly_tools_key.'_'.$skill_id,$occur_time);
+            $redis_battle->HDEL($fly_tools_key.'_'.$skill_id,$occur_time);
             // Log::info('out of time hdel skill_id'.$skill_id.' enemyX'.$enemyX.' enemyY'.$enemyY.' enemyskillXfrom'.$enemyX_from.' enemyskillXto'.$enemyX_to.' enemyskillYfrom'.$enemyY_from.' enemyskillYto'.$enemyY_to.' enemy_direction'.$enemy_direction.' userxfront'.$x_front.' useryfront'.$y_font.' user_xBack'.$x_back.' user_yBack'.$y_back.' userDirection'.$direction.'$current-$occur_time'.($current-$occur_time).'eff duration'.$effs['eff_duration']); 
          }
          // else if(!$hit&&$skill_damage==2&&$current-$occur_time<=$effs['eff_duration']){
