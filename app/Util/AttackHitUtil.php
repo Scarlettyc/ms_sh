4d3d5149ae579fd29d2205391447325b230a9f5b
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-05-31 17:50:18 +0800

    update battle

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -589,7 +589,8 @@ class AttackHitUtil
 		$user_def=($chardata['ch_armor']*1.1)/(15*$chardata['ch_lv']+$chardata['ch_armor']+40);
 		$enemy_res=$enemyData['ch_res'];
 		$hpMax=$chardata['ch_hp_max'];
-    Log::info('skill group '.$enemyData);
+    $enemyJson=json_encode($enemyData);
+    Log::info('skill group '.$enemyJson);
   		if($enemyData['skill']['skill_group']=1||$enemyData['skill']['skill_group']=5||$enemyData['skill']['skill_group']=6){
       if(!isset($skillatkEff['eff_skill_atk_point'])){
         $skillatkEff['eff_skill_atk_point']=3;
