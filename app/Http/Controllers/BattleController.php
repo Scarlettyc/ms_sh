7cde4ca3d8b91086125c794a1eca25eb7864830c
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-05 15:23:50 +0800

    test hash table

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -488,7 +488,7 @@ class BattleController extends Controller
 		$battlekey='battle_datam_1528168190_ui100000074';
 		$charData=$characterModel->select('ch_hp_max','ch_stam','ch_atk','ch_armor','ch_crit','ch_lv','ch_ranking','ch_res')->where('u_id','ui100000074')->first();
 		$this->addHash($charData,$current,'ui100000074');
-		echo $count;
+		//echo $count;
  	 // 	$clientInfo['address']='11111';
  	// 	// $clientInfo['port']='1222';
  		 
