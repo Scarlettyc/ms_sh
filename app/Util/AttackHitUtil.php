f2fd9022ca8f5dc14da41e812af989f223400776
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-07 17:40:55 +0800

    update battle

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -416,7 +416,7 @@ class AttackHitUtil
       $SkillEffDeatilModel=new SkillEffDeatilModel();
       $myBuffKey='mybuff'.$match_id.'_'.$u_id;
       if($buff==2){
-        $myBuffKey='debuff'.$match_id.'_'.$u_id;
+      $myBuffKey='debuff'.$match_id.'_'.$u_id;
       }
       $redis_battle=Redis::connection('battle');
       $buffData=$redis_battle->HGETALL($myBuffKey);
@@ -429,7 +429,9 @@ class AttackHitUtil
           $element_type=$keys[1];
           $elementTime=$SkillEffDeatilModel->select('skill_id','eff_value','eff_element_id')->where('skill_id',$pre_skill)->where('eff_type',$element_type)->where('eff_name','like','%time%')->first();
           $exist_time=$time+$elementTime['eff_value']-$current;
-          $result[]=['element_id'=>$element_type,'time'=>$exist_time];
+          if($exist_time>0){
+          $result[]=['element_id'=>$elementTime['eff_element_id'],'time'=>$exist_time];
+        }
       }
       return $result;
     }
