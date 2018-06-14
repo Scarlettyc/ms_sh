b5ce39391249a3eb65d8788bc03d4d105fed13f9
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-14 13:35:09 +0800

    update b04

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -300,7 +300,7 @@ class BattleController extends Controller
 					$charData['x']=$x;
 					$charData['y']=$y;
 					}else{
-						$charData['time']=$userData['x'];
+						$charData['time']=$userData['time'];
 						$charData['x']=$userData['x'];
 						$charData['y']=$userData['y'];
 						$charData['x2']=$userData['x2'];
