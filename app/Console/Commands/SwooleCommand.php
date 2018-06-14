6ebe0745fc39641a2679ccea3f0fff46790d7f2e
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-14 13:11:08 +0800

    update b04

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
 use swoole_server;
-use swoole_websocket_server;
 use App\Http\Controllers\BattleController;
 use Log;
 class SwooleCommand extends Command
@@ -41,8 +40,7 @@ class SwooleCommand extends Command
      */
 
     public function handle()
-    {  //$serv = new swoole_server("0.0.0.0/0", 6380, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
-        $serv = new swoole_websocket_server("0.0.0.0",6380,SWOOLE_BASE);
+    {   $serv = new swoole_server("0.0.0.0/0", 6380, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
         $serv->set(array(
             'worker_num'  => 8,
             'daemonize'   => 0, //是否作为守护进程,此配置一般配合log_file使用
