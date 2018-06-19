7e8e9f31621db76e575e93699d92a0203b681d35
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-19 18:30:28 +0800

    test data structure change

--- app/Http/Controllers/LoadBattleController.php
+++ app/Http/Controllers/LoadBattleController.php
@@ -84,7 +84,7 @@ class LoadBattleController extends Controller
         $user_def=($charData['ch_armor']*1.1)/(15*$charData['ch_lv']+$charData['ch_armor']+40);
         $user_def=round($user_def,2);
         var_dump($user_def);
-        $redis_battle_status->HSET($battle_status_key,'ch_def',0.89);
+        $redis_battle_status->HSET($battle_status_key,'ch_def',"0.89");
         
         $redis_battle_status->HSET($battle_status_key,'ch_hp_max',$charData['ch_hp_max']);
         $redis_battle_status->HSET($battle_status_key,'ch_crit',$charData['x2']);
