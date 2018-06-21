ba590705a35b51434819db1873620e79473ae9af
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-21 19:47:14 +0800

    simply multi

--- app/Util/AttackHitUtil.php
+++ app/Util/AttackHitUtil.php
@@ -362,7 +362,7 @@ class AttackHitUtil
           return $hit;
         }
       }
-  public function checkInterval($skill_id，$x,$y,$direction,$time,$skill_group,$skill_damage){
+  public function checkInterval($skill_id,$x,$y,$direction,$time,$skill_group,$skill_damage){
     $SkillEffDeatilModel=new SkillEffDeatilModel();
     $redis_battle_history=Redis::connection('battle');
     $multi_key='multi'.$match_id.$u_id;
