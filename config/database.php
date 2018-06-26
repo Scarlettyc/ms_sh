11b65173aa9cd73c0980db87ac3dd18a7e70feb5
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-06-26 12:55:02 +0800

    create test version

--- config/database.php
+++ config/database.php
@@ -119,13 +119,13 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 2,
+            'database' => 9,
         ],
         'battle_user' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 3,
+            'database' => 10,
         ],
         'options' => [
                 'parameters' => ['password' => env('REDIS_PASSWORD', null)],
