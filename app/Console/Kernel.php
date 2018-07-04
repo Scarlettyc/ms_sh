bd2286d45d56b499c6da37e04b7a0a86ec7d29cd
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-07-04 11:47:31 +0800

    test cron tab

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('battle:background')
-                 ->everyFiveMinutes();
+        // $schedule->command('battle:background')
+        //          ->everyFiveMinutes();
     }
 }
