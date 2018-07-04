b4ffe7db4660331c38639a7f169336d41445b949
Scarlettyc <yc_ms@yc-msdeMacBook-Pro.local>
2018-07-04 13:48:44 +0800

    test cron tab

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         // Commands\Inspire::class,
   \App\Console\Commands\SwooleCommand::class,
-\App\Console\Commands\NotifyCommand::class
+\App\Console\Commands\NotifyCommand::class,
+\App\Console\Commands\BattleBackgroundCommand::class
     ];
 
     /**
@@ -26,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('battle:background')
-        //          ->everyFiveMinutes();
+        $schedule->command('battle:background')
+                 ->everyFiveMinutes();
     }
 }
