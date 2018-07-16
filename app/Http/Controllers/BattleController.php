b9805ddb44fc0e61c7bf5a4a7d415cd37e2cfeef
Scarlettyc <yuchen_liu_sh@163.com>
2018-07-16 15:51:41 +0800

    update battle

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -329,30 +329,32 @@ class BattleController extends Controller
  		$now   = new DateTime;
 		$dmy=$now->format( 'Ymd' );
 		$u_id=$result['u_id'];
-		$battleKey='battle_status'.$u_id.$dmy;
- 	   	$match_id=$redis_battle->HGET($battleKey,'match_id');
- 	   	$enemy_uid=$redis_battle->HGET($battleKey,'enemy_uid');
-		$current=$this->getMillisecond();
-		$playerData=$result['playerData'];
-		$frame_id=$result['frame_id'];
-		$frameKey='battle_status'.$u_id.$match_id;
-		$frameData=json_encode($playerData,TRUE);
-		$redis_user->HSET($frameKey,$frame_id,$frameData);
-		$enemyFramekey='battle_status'.$enemy_uid.$match_id;
-		$enmeyFrameData=$redis_user->HGET($enemyFramekey,$frame_id);
-		// if(!$enmeyFrameData){
-		// 	throw new Exception("lost this frame data", 1);
-			
-		// }
-		if($frameData&&$enmeyFrameData){
-		$result['battle_data'][]=$frameData;
-		$result['battle_data'][]=$enmeyFrameData;
-		$result['frame_id']=$frame_id;
+		if($u_id){
+				$battleKey='battle_status'.$u_id.$dmy;
+		 	   	$match_id=$redis_battle->HGET($battleKey,'match_id');
+		 	   	$enemy_uid=$redis_battle->HGET($battleKey,'enemy_uid');
+				$current=$this->getMillisecond();
+				$playerData=$result['playerData'];
+				$frame_id=$result['frame_id'];
+				$frameKey='battle_data'.$u_id.$match_id;
+				$frameData=json_encode($playerData,TRUE);
+				$redis_user->HSET($frameKey,$frame_id,$frameData);
+				$enemyFramekey='battle_data'.$enemy_uid.$match_id;
+				$enmeyFrameData=$redis_user->HGET($enemyFramekey,$frame_id);
+				// if(!$enmeyFrameData){
+				// 	throw new Exception("lost this frame data", 1);
+					
+				// }
+				if($frameData&&$enmeyFrameData){
+				$result['battle_data'][]=$frameData;
+				$result['battle_data'][]=$enmeyFrameData;
+				$result['frame_id']=$frame_id;		
 
-		$response=json_encode($result,TRUE);
-		Log::info($response);
-		return  $response;
-		}
+				$response=json_encode($result,TRUE);
+				Log::info($response);
+				return  $response;
+				}
+			}
 
 	}
 
