c4352daae53dea46e8d0bfffa761c1b1c9b9eb78
Scarlettyc <yuchen_liu_sh@163.com>
2018-07-16 19:05:56 +0800

    update battle

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -359,14 +359,13 @@ class BattleController extends Controller
 			$enemyFramekey='battle_data'.$enemy_uid.$match_id;
 			$enmeyFrameData=$redis_user->HGET($enemyFramekey,$frame_id);
 			
-			if($enmeyFrameData){
 			$result['battle_data'][]=$frameData;
+			if($enmeyFrameData){
 			$result['battle_data'][]=$enmeyFrameData;
+			}
 			$result['frame_id']=$frame_id;	
 			$response=json_encode($result,TRUE);
-			Log::info($response);
 			return 	$response;
-			}
 	}
 
 	private function removeUsedSkill($u_id){
