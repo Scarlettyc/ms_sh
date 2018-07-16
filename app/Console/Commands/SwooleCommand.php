34ac95d64b6317ca16259cdf5c840a403ea33ba7
Scarlettyc <yuchen_liu_sh@163.com>
2018-07-16 19:46:10 +0800

    update battle

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -103,6 +103,7 @@ class SwooleCommand extends Command
         $serv->on('Packet', function ($serv, $data, $clientInfo) {
              $serv->tick(100, function() use ($serv, $data,$clientInfo) {
                 $battle=new BattleController();
+                Log::info($data);
                 $arr=json_decode($data,TRUE);
                 $result=$battle->battleTestNew($arr,$clientInfo);
                 if($result==1){
