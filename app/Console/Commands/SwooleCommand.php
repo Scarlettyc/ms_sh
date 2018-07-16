40b8551e0aa034461a9696bf985aec796fbd230f
Scarlettyc <yuchen_liu_sh@163.com>
2018-07-16 21:32:39 +0800

    update battle

--- app/Console/Commands/SwooleCommand.php
+++ app/Console/Commands/SwooleCommand.php
@@ -140,7 +140,7 @@ class SwooleCommand extends Command
              } );
 
         $serv->start(){
-              Log::info("test udp start");
+             Log::info("test udp start");
              $serv->tick(600, function() {
                 $battle=new BattleController();
                 //$arr=json_decode($data,TRUE);
