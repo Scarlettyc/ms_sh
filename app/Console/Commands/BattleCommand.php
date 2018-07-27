7a3a4e2cb2ddc34c9bab226a02bf9c9e2dd87ec3
Scarlettyc <yuchen_liu_sh@163.com>
2018-07-27 18:22:35 +0800

    test battle

--- app/Console/Commands/BattleCommand.php
+++ app/Console/Commands/BattleCommand.php
@@ -84,10 +84,10 @@ class BattleCommand extends Command
                         $server->tick(600, function()use($u_id, $match_id,$frame_id,$BattleController,$server) {
                             $frame_id=$frame_id+1;
                             Log::info("test tick 667");
-                            $resultList=$BattleController->battleReturn($u_id,$match_id,$frame_id);
-                          //     $server->push($resultList['client_id_2'], $resultList['battle_data']); 
-                          //   $server->push($resultList['client_id'], $resultList['battle_data']);
-                          // });
+                            // $resultList=$BattleController->battleReturn($u_id,$match_id,$frame_id);
+                            //   $server->push($resultList['client_id_2'], $resultList['battle_data']); 
+                            // $server->push($resultList['client_id'], $resultList['battle_data']);
+                          });
  
                      }
                      if($uslist[0]=="BattleRecieve"){
