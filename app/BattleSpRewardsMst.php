3e220b1c46244e3cebcf2861e4c157c937d9d401
Scarlettyc <yuchen_liu_sh@163.com>
2018-08-06 11:22:40 +0800

    fix swoole

--- app/BattleSpRewardsMst.php
+++ app/BattleSpRewardsMst.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BattleSpRewardsMst extends Model
 {
-	protected $fillable = ['r_id','map_id','ranking','lv','item_org_id','item_type','item_quantity','item_rate_from','item_rate_to','start_date','end_date','createdate','updated_at'];
+	protected $fillable = ['r_id','map_id'，'lv','item_org_id','item_type','item_quantity','item_rate_from','item_rate_to','start_date','end_date','createdate','updated_at'];
 
 	protected $connection = 'mysql';
 	protected $table = "Battle_speical_rewards_mst";
