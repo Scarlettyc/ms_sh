e3a8b5dd54cfc225595c92f3dbf27a7092b7bae4
Scarlettyc <yuchen_liu_sh@163.com>
2018-08-07 17:54:18 +0800

    udpate shop

--- app/MissionListMstModel.php
+++ app/MissionListMstModel.php
@@ -7,7 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 class MissionListMstModel extends Model
 {
-	protected $fillable = ['mission_id' ,'mission_type' ,'user_lv_from','user_lv_to','ranking_from','ranking_to','times','description','start_date','end_date','createdate','updated_at'];
+	protected $fillable = ['mission_id' ,'mission_type' ,'user_lv_from','user_lv_to','times','description','start_date','end_date','createdate','updated_at'];
 
 	protected $connection = 'mysql';
 	protected $table = "Mission_List_mst";
